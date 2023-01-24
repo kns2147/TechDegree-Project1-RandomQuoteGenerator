fe724942110451af6fd06511b6811cd307467fd4
kns2147 <123277319+kns2147@users.noreply.github.com>
2023-01-23 21:12:15 -0800

    js file updated and practice.html file deleted

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ Katherine Seibel, 2023
 
 var quotes = [
   { 
-    quote: '"Don\'t wait until you\'ve reached your goal to be proud of yourself. Be proud of every step you take toward reaching that goal." ',
+    quote: 'Don\'t wait until you\'ve reached your goal to be proud of yourself. Be proud of every step you take toward reaching that goal.',
     source: 'Simone Biles',
     citation: '',
     year:'',
@@ -24,50 +24,46 @@ var quotes = [
   },
 
   { 
-    quote: '“Breathe. Let go. And remind yourself that this very moment is the only one you know you have for sure.” ',
+    quote: 'Breathe. Let go. And remind yourself that this very moment is the only one you know you have for sure.',
     source: 'Oprah Winfrey',
     citation: '',
     year:'',
     occupation: 'Talk-Show Host'
   },
   { 
-    quote: '“I get angry about things, then go on and work.” ',
+    quote: 'I get angry about things, then go on and work.',
     source: 'Toni Morrison',
     citation: '',
     year:'',
     occupation: 'Author'
   },
   { 
-    quote: '“It is so liberating to really know what I want, what truly makes me happy, what I will not tolerate. I have learned that it is no one else\'s job to take care of me but me." ',
+    quote: 'It is so liberating to really know what I want, what truly makes me happy, what I will not tolerate. I have learned that it is no one else\'s job to take care of me but me.',
     source: 'Beyonce Knowles',
     citation: '',
     year:'',
     occupation: 'Singer-Songwriter'
   },
   { 
-    quote: '“When I dare to be powerful — to use my strength in the service of my vision, then it becomes less and less important whether I am afraid.” ',
+    quote: 'When I dare to be powerful — to use my strength in the service of my vision, then it becomes less and less important whether I am afraid.',
     source: 'Audre Lorde',
     citation: '',
     year:'',
     occupation: 'Feminist and Civil Rights Activist'
   },
   { 
-    quote: '“Hate is too great a burden to bear. It injures the hater more than it injures the hated.” ',
-    source: 'Corretta Scott King',
+    quote: 'Hate is too great a burden to bear. It injures the hater more than it injures the hated.',
+    source: 'Coretta Scott King',
     citation: '',
     year:'',
-    tag: 'Civil Rights Activist'
+    occupation: 'Civil Rights Activist'
   },
 ];
 
-
 /***
- * `getRandomQuote` function
- * The getRandomQuote function creates a random number. 
- * In the function, create a variable to store a random number. 
+ *The `getRandomQuote` function creates a random number. 
  * The random number is used to return a random quote object from the quotes array.
 ***/
-
 
 function getRandomQuote (){
   const randomNumber = Math.floor(Math.random()*(quotes.length));
@@ -75,22 +71,38 @@ function getRandomQuote (){
   return quote;
 }
 
-console.log(getRandomQuote());
-
-getRandomQuote();
+//getRandomQuote(console.log(getRandomQuote()));
 
 /***
- * `printQuote` function
+ *The `printQuote` function provides a radom quote to display in the browser.
 ***/
+
  function printQuote (){
-  getRandomQuote();
+  const quoteToDisplay = getRandomQuote();
+  const html = 
+    `<p class="quote"> ${quoteToDisplay.quote}</p>
+    <p class="source">${quoteToDisplay.source}<span class="occupation">, ${quoteToDisplay.occupation}</span>
+    </p>`;
+    return html;
+}
 
+document.getElementById('quote-box').innerHTML = printQuote(); 
 
- }
+console.log(printQuote());
+
+/***
+ * The `randomColor`function provides a random background color for the page in the browser.
+ */
+let htmlBackground = '';
+const randomValueForColor = ()=>Math.floor(Math.random()*256);
+
+function randomColor(){
+  let color = 'rgb(${randomValueForColor()},${randomValueForColor()},${randomValueForColor()})';
+  document.body.style.backgroundColor = color;
+}
 
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
-
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
