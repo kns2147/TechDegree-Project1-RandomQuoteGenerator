6db3929412bf9fb01e100ee0e7f36a44e629eaa0
kns2147 <123277319+kns2147@users.noreply.github.com>
2023-01-25 20:12:36 -0800

    updates to code

--- js/script.js
+++ js/script.js
@@ -17,44 +17,34 @@ var quotes = [
   { 
     quote: 'Don\'t wait until you\'ve reached your goal to be proud of yourself. Be proud of every step you take toward reaching that goal.',
     source: 'Simone Biles',
-    citation: '',
     year:'2022',
     occupation: 'Olympic Gymnast'
   },
-
   { 
     quote: 'Breathe. Let go. And remind yourself that this very moment is the only one you know you have for sure.',
     source: 'Oprah Winfrey',
-    citation: '',
-    year:'',
     occupation: 'Talk-Show Host'
   },
   { 
-    quote: 'I get angry about things, then go on and work.',
+    quote: 'We die. That may be the meaning of life. But we do language. That may be the measure of our lives.',
     source: 'Toni Morrison',
-    citation: '',
-    year:'',
+    citation: 'Nobel Lecture',
+    year:'1993',
     occupation: 'Author'
   },
   { 
     quote: 'It is so liberating to really know what I want, what truly makes me happy, what I will not tolerate. I have learned that it is no one else\'s job to take care of me but me.',
     source: 'Beyonce Knowles',
-    citation: '',
-    year:'',
     occupation: 'Singer-Songwriter'
   },
   { 
     quote: 'When I dare to be powerful — to use my strength in the service of my vision, then it becomes less and less important whether I am afraid.',
     source: 'Audre Lorde',
-    citation: '',
-    year:'',
     occupation: 'Feminist and Civil Rights Activist'
   },
   { 
     quote: 'Hate is too great a burden to bear. It injures the hater more than it injures the hated.',
     source: 'Coretta Scott King',
-    citation: '',
-    year:'',
     occupation: 'Civil Rights Activist'
   },
 ];
@@ -78,10 +68,11 @@ function getRandomQuote (){
   let quoteToDisplay = getRandomQuote();
   let html = 
     `<p class="quote"> ${quoteToDisplay.quote} </p>
-    <p class="source">${quoteToDisplay.source}
+    <p class="source">${quoteToDisplay.source}, 
+      <span class="occupation">${quoteToDisplay.occupation}</span>
     `
-      if (quoteToDisplay.occupation) {
-        html+= `<span class="occupation">, ${quoteToDisplay.occupation}</span>`
+      if (quoteToDisplay.citation) {
+        html+= `<span class="citation">${quoteToDisplay.citation}</span>`
       }
       if(quoteToDisplay.year) {
         html+=`<span class="year">${quoteToDisplay.year}</span>`
@@ -95,20 +86,25 @@ function getRandomQuote (){
 
 /***
  * The `randomColor`function provides a random background color for the page in the browser.
+ * It's not functioning and I can't figure out what is wrong so any feedback, I would really appreciate.
  ***/
 
-let randomValueForColor = ()=>Math.floor(Math.random()*256);
 
 function randomColor(){
-  let color = 'rgb(${randomValueForColor}, {randomValueForColor}, {randomValueForColor})';
-  document.body.style.backgroundColor = color;
+  let randomValueForColor1 = Math.floor(Math.random()*256);
+  let randomValueForColor2 = Math.floor(Math.random()*256);
+  let randomValueForColor3 = Math.floor(Math.random()*256);
+  let color = 'rgb(${randomValueForColor1}, ${randomValueForColor2}, ${randomValueForColor3})';
+  return color;
 }
+
+document.body.style.backgroundColor = randomColor();
 
 /***
  * The `autoRefresh`variable provides the setInterval() method to refresh the quote on the page in the browser.
  ***/
 
-let autoRefresh = setInterval(printQuote, 1000);
+let autoRefresh = setInterval(printQuote, 2000);
 
 /***
  * click event listener for the print quote button
